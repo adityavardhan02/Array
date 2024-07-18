30224c17872adc3d095f5fd35ec196a414001922
Aditya vardhan <adityadannapaneni@gmail.com>
2024-07-18 19:26:53 +0530

    Add files via upload

---
+++ ArrayElements.java
@@ -0,0 +1,31 @@
+package apjfsa;
+
+public class ArrayElements {
+
+	int[] arr;
+
+	public ArrayElements(int[] arr) {
+		super();
+		this.arr = arr;
+	}
+
+	synchronized void sumOfElements() {
+		int sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			sum += arr[i];
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(arr[i]);
+		}
+		System.out.println("Sum="+sum);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
