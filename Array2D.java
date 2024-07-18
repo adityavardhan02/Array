3446f57c6db51c545571c13d84026290c7dc8a75
Aditya vardhan <adityadannapaneni@gmail.com>
2024-07-18 19:29:49 +0530

    Add files via upload

---
+++ Array2D.java
@@ -0,0 +1,33 @@
+package anudip;
+
+public class Array2D {
+
+	public static void main(String[] args) {
+		int[][] arr=new int[2][2];
+		arr[0][0]=10;
+		arr[0][1]=11;
+		arr[1][0]=12;
+		arr[1][1]=13;
+		
+		int i,j;
+		
+		for (i=0;i<2;i++)//row
+		{
+			for(j=0;j<2;j++)//column 
+			
+				System.out.println(arr[i][j]+" ");
+			System.out.println();
+				
+				
+			}
+				
+	
+	int[][] arr1= {{1,2,3},{2,3,4}};
+	{
+	
+	System.out.println("No.of rows in arr1="+arr1.length);
+	System.out.println("No.of columns in arr1"+arr1[0].length);
+
+		}
+}
+}
